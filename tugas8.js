9b5a5bd7758d1e0f798d99c74853628d3fdd0b3f
yohanes_prima <jooprima@gmail.com>
2019-07-14 22:39:30 +0700

    Tugas 8 js
    tugas ini diumpulkan untuk NIOMIC

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function panggilObject() {
+  var warnaPakaian = {
+    baju: "merah",
+    celana: "hitam",
+    jaket: "putih"
+  };
+  warnaPakaian.baju = "kuning";
+  console.log(warnaPakaian);
+}
+
+panggilObject();
